Fixed a small bug in the calculation of lambda_r1, updated unit tests to reflect the bugfix.

src/birkeland/__init__.py
@@ -85,7 +85,7 @@ class Model(object):
 
     def lambda_r1(self):
         """lambda_R1 determined using F_PC from the inverse of Equation 8."""
-        return np.sqrt(np.arcsin(self.f_pc / (2 * np.pi * (self._r_e ** 2) * self._b_eq)))
+        return np.arcsin(np.sqrt(self.f_pc / (2 * np.pi * (self._r_e ** 2) * self._b_eq)))
 
     def v_r1(self):
         """R1 current oval velocity V_R1 from Equation 9."""

tests/test.py
@@ -1,15 +1,13 @@
 import numpy as np
 from birkeland import Model
-from pandas import DataFrame, read_csv
+from pandas import read_csv
 from pathlib import Path
 from unittest import TestCase
-
-test_directory = Path(__file__).parent
 
 
 class TestBirkeland(TestCase):
     def test_against_data(self):
-        benchmarks = read_csv(test_directory / "test_data.csv")
+        benchmarks = read_csv(Path(__file__).parent / "test_data.csv")
         examples = []
         for cnt, row in benchmarks.iterrows():
             examples.append(Model(row["phi_d"], row["phi_n"], row["f_pc"]))
@@ -25,45 +23,6 @@ class TestBirkeland(TestCase):
                                        err_msg=f"{key} mean")
             np.testing.assert_allclose(data[f"{key}_sum"], benchmarks[f"{key}_sum"].values,
                                        err_msg=f"{key} sum")
-
-
-def create_test_csv(filepath):
-    """
-    Create a csv file which can be used for unit testing the Model class.
-
-    Parameters
-    ----------
-    filepath : pathlib.Path
-        The filepath of the csv file to act as benchmarks for future unit testing.
-    """
-    examples = np.empty((6, 6, 5), dtype=object)
-    phi_d_values = np.arange(0, 60, 10)
-    phi_n_values = np.arange(0, 60, 10)
-    f_pc_values = np.arange(0.1, 1.1, 0.2)
-
-    test_data = {"phi_d": [],
-                 "phi_n": [],
-                 "f_pc": []}
-
-    for cnt_d, phi_d in enumerate(phi_d_values):
-        for cnt_n, phi_n in enumerate(phi_n_values):
-            for cnt_f, f_pc in enumerate(f_pc_values):
-                examples[cnt_d, cnt_n, cnt_f] = Model(phi_d, phi_n, f_pc)
-
-                test_data["phi_d"].append(phi_d)
-                test_data["phi_n"].append(phi_n)
-                test_data["f_pc"].append(f_pc)
-
-    examples = examples.flatten()
-
-    for key in test_data:
-        test_data[key] = np.array(test_data[key])
-
-    test_dictionary, _, _ = create_test_set(examples)
-    test_data.update(test_dictionary)
-
-    test_data = DataFrame(test_data)
-    test_data.to_csv(filepath)
 
 
 def create_test_set(examples):