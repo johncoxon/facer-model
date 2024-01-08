11788f51fb6272becc148531330382c1c9b75e0b
John Coxon <john@johncoxon.co.uk>
2024-01-08 11:06:39 +0000

    Minor updates.

--- src/birkeland/__init__.py
+++ src/birkeland/__init__.py
@@ -744,20 +744,15 @@ class Model(BaseModel):
 
 
 class DailyAverage(object):
-    def __init__(self, phi_n, f_107, day, hemisphere, **kwargs):
+    def __init__(self, phi_d, f_107, day, hemisphere, **kwargs):
         """
         The expanded Milan (2013) model calculated at both UT=5 and UT=17 for the input day and then averaged.
         This simplification means that the dayside and nightside reconnection rates can be assumed to be approximately
-        equal and so only
-
-        The Milan (2013) model expanded with the Moen and Brekke (1993) model of quiet-time
-        conductance as employed by Coxon et al. (2016).
-
-        kwargs are passed onto the underlying BaseModel class.
+        equal and so only the dayside reconnection rate need be provided.
 
         Parameters
         ----------
-        phi_n : float
+        phi_d : float
             Reconnection rates, in kV.
         f_107 : float
             The F10.7 index, in solar flux units.
@@ -767,7 +762,7 @@ class DailyAverage(object):
         if day.hour != 0 or day.minute != 0 or day.second != 0 or day.microsecond != 0:
             raise ValueError("The day must not have any associated time information.")
 
-        self.ut_5 = Model(phi_n, phi_n, f_107, day + timedelta(hours=5), hemisphere, **kwargs)
-        self.ut_17 = Model(phi_n, phi_n, f_107, day + timedelta(hours=17), hemisphere, **kwargs)
+        self.ut_5 = Model(phi_d, phi_d, f_107, day + timedelta(hours=5), hemisphere, **kwargs)
+        self.ut_17 = Model(phi_d, phi_d, f_107, day + timedelta(hours=17), hemisphere, **kwargs)
 
-        self.j = np.mean(self.ut_5.j, self.ut_17.j)
+        self.j = np.mean((self.ut_5.j, self.ut_17.j))
